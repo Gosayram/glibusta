8edb115e8b7111b9d73f4893b670115f68317d6d
Abdurakhman Rakhmankulov <joha.shadibekov@gmail.com>
2026-06-06 21:53:38 +0500

    [UPD] - recreate project basic;

--- glibusta/lib/core/config/app_config.dart
+++
@@ -1,9 +0,0 @@
-class AppConfig {
-  static const String appName = 'Glibusta';
-  static const String defaultBaseUrl = 'http://flibusta.site';
-  static const List<String> mirrors = [
-    'http://flibusta.site',
-    'http://flibusta.is',
-    'http://flibusta.online',
-  ];
-}
